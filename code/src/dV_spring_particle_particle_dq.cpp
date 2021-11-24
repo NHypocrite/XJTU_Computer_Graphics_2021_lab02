04b2962612ec7d5179ae0ff475ca8e28a990d5fb
NHypocrite's Ubuntu <zmdhpc@163.com>
2021-11-24 12:27:44 +0800

    Updated the function force and stiff and their comments.

--- code/src/dV_spring_particle_particle_dq.cpp
+++ code/src/dV_spring_particle_particle_dq.cpp
@@ -1,7 +1,8 @@
 #include <dV_spring_particle_particle_dq.h>
 // 使用 k(即stiffness)和 x(即q-l)计算 f ，存储在引用的第一个参数中
 void dV_spring_particle_particle_dq(Eigen::VectorXd &dV, const Eigen::VectorXd &q, double stiffness) {
-    //dV.resize(1);
+
+    dV.resize(1);           // f为一个数，当存储在一个1x1的矩阵中
     //compute f
-    //dV = q * stiffness;// f = k * x
+    dV = stiffness * (q-l); // f = k * x
 }
