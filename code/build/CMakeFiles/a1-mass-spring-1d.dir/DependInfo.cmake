36d714ae7ddfcb856b0d5c3abf1f74ddfd101c3e
NHypocrite's Ubuntu <zmdhpc@163.com>
2021-11-24 11:27:17 +0800

    Added the original version of the code.

---
+++ code/build/CMakeFiles/a1-mass-spring-1d.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xjtuhpc/Code/Lab2_me/main.cpp" "/home/xjtuhpc/Code/Lab2_me/build/CMakeFiles/a1-mass-spring-1d.dir/main.cpp.o"
+  "/home/xjtuhpc/Code/Lab2_me/shared/src/visualization.cpp" "/home/xjtuhpc/Code/Lab2_me/build/CMakeFiles/a1-mass-spring-1d.dir/shared/src/visualization.cpp.o"
+  "/home/xjtuhpc/Code/Lab2_me/src/d2V_spring_particle_particle_dq2.cpp" "/home/xjtuhpc/Code/Lab2_me/build/CMakeFiles/a1-mass-spring-1d.dir/src/d2V_spring_particle_particle_dq2.cpp.o"
+  "/home/xjtuhpc/Code/Lab2_me/src/dV_spring_particle_particle_dq.cpp" "/home/xjtuhpc/Code/Lab2_me/build/CMakeFiles/a1-mass-spring-1d.dir/src/dV_spring_particle_particle_dq.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../shared/include"
+  "../include"
+  "../extern/libigl/include"
+  "../extern/libigl/external/glad/include"
+  "../extern/libigl/external/glfw/include"
+  "../extern/libigl/external/libigl-imgui/."
+  "../extern/libigl/external/libigl-imgui/.."
+  "../extern/libigl/external/libigl-imgui/../imgui/examples"
+  "../extern/libigl/cmake/../include"
+  "../extern/libigl/cmake/../external/eigen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xjtuhpc/Code/Lab2_me/build/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  "/home/xjtuhpc/Code/Lab2_me/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/xjtuhpc/Code/Lab2_me/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/src/dV_spring_particle_particle_dq.cpp
@@ -0,0 +1,7 @@
+#include <dV_spring_particle_particle_dq.h>
+
+void dV_spring_particle_particle_dq(Eigen::VectorXd &dV, const Eigen::VectorXd &q, double stiffness) {
+    //dV.resize(1);
+    //compute f
+    //dV = q * stiffness;// f = k * x
+}
